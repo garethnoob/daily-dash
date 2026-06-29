94fe2bb71ef367cad12cb8a7b4bcf734e9775669
Gareth Davies <gaz.andrew@gmail.com>
2026-06-29 19:36:57 +0100

    first commit

---
+++ daily-dash/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>

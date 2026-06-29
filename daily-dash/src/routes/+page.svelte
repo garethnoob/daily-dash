e3acd539c30c64b4f6b9db1a0cd2e29ebeab19b6
Gareth Davies <gaz.andrew@gmail.com>
2026-06-29 19:48:04 +0100

    Update welcome message in +page.svelte for personalization

--- daily-dash/src/routes/+page.svelte
+++ daily-dash/src/routes/+page.svelte
@@ -3,5 +3,5 @@
 	<a href="/about">about</a>
 </nav>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit you beautiful boy</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
